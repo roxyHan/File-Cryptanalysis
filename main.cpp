45505da6b5e6e6bb9fb4fe79123edfccdf7a63a8
roxyHan <djelia96@gmail.com>
2020-04-28 17:58:45 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "The name of the file is required! Please enter the file name to proceed.." << endl;
+        return EXIT_FAILURE;
+    }
+    // Get the filename
+    string filename = argv[1];
+
+    // Read the content of the file
+
+
+
+
+    return 0;
+}
